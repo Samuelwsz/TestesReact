8c406f50feed79202cec81d436be60f98eb9ac36
samuelwsz <samuelxwsz@gmail.com>
2023-09-27 11:52:47 -0300

    att

--- src/state/hook/errorMessage.ts
+++
@@ -1,7 +0,0 @@
-import { useRecoilValue } from "recoil"
-import { errorState } from "../atom"
-
-export const useErrorMessage = () => {
-  const message = useRecoilValue(errorState)
-  return message
-}
